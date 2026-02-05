7283c3042053ed51b09f3d9676b1fd98db517ba7
GeethaEswaramoorthy <esgeethu109@gmail.com>
2026-02-05 17:58:16 +0530

    css update and logo

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -7,14 +7,13 @@ function Header() {
   return (
     <div className="header">
       <img height={100}
-      // className="header-image" 
       src={logo} alt="Logo" />
       <h1 className="logo-name">Geetha Boutique</h1>
 
       <div>
         <Link className="Menu" to="/">Home</Link>
         <Link className="Menu" to="/collection">Collections</Link>
-        <Link className="Menu" to="/about">About Us</Link>
+        <Link className="Menu" to="/about">AboutUs</Link>
         <Link className="Menu" to="/contact">Contact</Link>
       </div>
     </div>
